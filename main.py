3ea5db2f8f57197c5b0227a4579c999138dff812
kevzter18 <kevzter18@gmail.com>
2018-11-08 14:35:52 +0800

    can now access the database sorry it's so slow

--- main.py
+++ main.py
@@ -3,12 +3,32 @@ from room import Room
 from course import Course
 from offering import Offering
 
-import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  passwd="root"
-)
+#!/usr/bin/python3
 
-print(mydb)
+import pymysql
+
+# Open database connection
+db = pymysql.connect("localhost","root","root","mysql" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Prepare SQL query to INSERT a record into the database.
+sql = "SELECT * FROM timetabling.course"
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Fetch all the rows in a list of lists.
+   results = cursor.fetchall()
+   for row in results:
+      fname = row[4]
+
+      # Now print fetched result
+      print ("fname = %s" % \
+         (fname ))
+except:
+   print ("Error: unable to fetch data")
+
+# disconnect from server
+db.close()
