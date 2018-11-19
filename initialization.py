6fdd635538af8e78ef7ad306004a05c2516f0e5b
kevzter18 <kevzter18@gmail.com>
2018-11-19 15:38:56 +0800

    temp_list???

--- initialization.py
+++ initialization.py
@@ -4,33 +4,40 @@ import random
 
 def initialize(faculty_list, offering_list):
 
+    temp_list = faculty_list.copy()
     for item in offering_list:
         boolean = False
         while not boolean:
-            rand = random.randint(0, len(faculty_list)-1)
-            if faculty_list[rand].load >= item.units:
+            rand = random.randint(0, len(temp_list)-1)
+            if temp_list[rand].load >= item.units:
+                # Changing value of faculty_list
+                for item2 in faculty_list:
+                    if item2.professor_id == temp_list[rand].professor_id:
+                        item.professor_id = item2.professor_id
+                        item2.load -= item.units
+                        item2.units += item.units
+                '''
                 item.professor_id = faculty_list[rand].professor_id
                 faculty_list[rand].load -= item.units
                 faculty_list[rand].units += item.units
+                '''
+                temp_list[rand].load -= item.units
+                temp_list[rand].units += item.units
+            elif temp_list[rand].load < item.units:
+                del  temp_list[rand]
+                if len(temp_list) == 0:
+                    break
             else:
                 boolean = True
 
 
-            ''' 
-            try:
-                rand = random.randint(0, len(faculty_list)-1)
-            if faculty_list[rand].load > item.units:
-                item.professor_id = faculty_list[rand].professor_id
-                faculty_list[rand].units += item.units
-                faculty_list[rand].load -= item.units
-                boolean = 1
-            '''
 
 
     for object in offering_list:
         print(object.professor_id, object.course_code, object.section)
     for object2 in faculty_list:
         print(object2.professor_id, object2.first_name, object2.last_name, object2.load, object2.units)
+    print (len(faculty_list))
 
 
 
