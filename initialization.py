1837ab91583110b24fe23b6897c412c0f788c32b
kevzter18 <kevzter18@gmail.com>
2019-03-24 14:37:53 +0800

    Made the code easier to read by removing the numbers in the for loops of printing lists cause I didn't know it was okay to make them the same

--- initialization.py
+++ initialization.py
@@ -24,68 +24,71 @@ def initialize(faculty_list, offering_list, timeslot_list):
         print(object.professor_id, object.course_code, object.section)
     print("\n")
 # Display Faculty with Offerings assigned
-    for object2 in faculty_list:
-        print(object2.professor_id, object2.first_name, object2.last_name, object2.load, object2.units)
+    for object in faculty_list:
+        print(object.professor_id, object.first_name, object.last_name, object.load, object.units)
     print("\n")
 
 
 
 
 # Assign day timeslots to faculty's offerings
-    for item2 in offering_list:
+    for item in offering_list:
         boolean = False
         while not boolean:
             rand = random.randint(0, len(timeslot_list) - 1)
-            if item2.course_type == timeslot_list[rand].room_type and item2.max_students == timeslot_list[rand].room_capacity and timeslot_list[rand].offering_id == "":
-                if item2.units == 1:
-                    timeslot_list[rand].offering_id = item2.offering_id
-                    item2.timeslot1_id = timeslot_list[rand].timeslot_id
-                    item2.room_id = timeslot_list[rand].room_code
+            if item.course_type == timeslot_list[rand].room_type and item.max_students == timeslot_list[rand].room_capacity and timeslot_list[rand].offering_id == "":
+                if item.units == 1:
+                        timeslot_list[rand].offering_id = item.offering_id
+                        item.timeslot1_id = timeslot_list[rand].timeslot_id
+                        item.room_id = timeslot_list[rand].room_code
 
-                elif item2.units == 3:
+
+
+                elif item.units == 3:
                     if  timeslot_list[rand].class_day == 'M' and timeslot_list[rand+1].offering_id == "":
-                        timeslot_list[rand].offering_id = item2.offering_id
-                        timeslot_list[rand+1].offering_id = item2.offering_id
-                        item2.timeslot1_id = timeslot_list[rand].timeslot_id
-                        item2.timeslot2_id = timeslot_list[rand+1].timeslot_id
-                        item2.room_id = timeslot_list[rand].room_code
+                        timeslot_list[rand].offering_id = item.offering_id
+                        timeslot_list[rand+1].offering_id = item.offering_id
+                        item.timeslot1_id = timeslot_list[rand].timeslot_id
+                        item.timeslot2_id = timeslot_list[rand+1].timeslot_id
+                        item.room_id = timeslot_list[rand].room_code
 
                     elif timeslot_list[rand].class_day == 'W' and timeslot_list[rand-1].offering_id == "":
-                        timeslot_list[rand].offering_id = item2.offering_id
-                        timeslot_list[rand-1].offering_id = item2.offering_id
-                        item2.timeslot1_id = timeslot_list[rand-1].timeslot_id
-                        item2.timeslot2_id = timeslot_list[rand].timeslot_id
-                        item2.room_id = timeslot_list[rand].room_code
+                        timeslot_list[rand].offering_id = item.offering_id
+                        timeslot_list[rand-1].offering_id = item.offering_id
+                        item.timeslot1_id = timeslot_list[rand-1].timeslot_id
+                        item.timeslot2_id = timeslot_list[rand].timeslot_id
+                        item.room_id = timeslot_list[rand].room_code
 
                     elif timeslot_list[rand].class_day == 'T' and timeslot_list[rand+1].offering_id == "":
-                        timeslot_list[rand].offering_id = item2.offering_id
-                        timeslot_list[rand+1].offering_id = item2.offering_id
-                        item2.timeslot1_id = timeslot_list[rand].timeslot_id
-                        item2.timeslot2_id = timeslot_list[rand+1].timeslot_id
-                        item2.room_id = timeslot_list[rand].room_code
+                        timeslot_list[rand].offering_id = item.offering_id
+                        timeslot_list[rand+1].offering_id = item.offering_id
+                        item.timeslot1_id = timeslot_list[rand].timeslot_id
+                        item.timeslot2_id = timeslot_list[rand+1].timeslot_id
+                        item.room_id = timeslot_list[rand].room_code
 
                     elif timeslot_list[rand].class_day == 'H'and timeslot_list[rand-1].offering_id == "":
-                        timeslot_list[rand].offering_id = item2.offering_id
-                        timeslot_list[rand-1].offering_id = item2.offering_id
-                        item2.timeslot1_id = timeslot_list[rand - 1].timeslot_id
-                        item2.timeslot2_id = timeslot_list[rand].timeslot_id
-                        item2.room_id = timeslot_list[rand].room_code
+                        timeslot_list[rand].offering_id = item.offering_id
+                        timeslot_list[rand-1].offering_id = item.offering_id
+                        item.timeslot1_id = timeslot_list[rand - 1].timeslot_id
+                        item.timeslot2_id = timeslot_list[rand].timeslot_id
+                        item.room_id = timeslot_list[rand].room_code
 
                     elif timeslot_list[rand].class_day == 'F' and timeslot_list[rand+1].offering_id == "":
-                        timeslot_list[rand].offering_id = item2.offering_id
-                        timeslot_list[rand+1].offering_id = item2.offering_id
-                        item2.timeslot1_id = timeslot_list[rand].timeslot_id
-                        item2.timeslot2_id = timeslot_list[rand+1].timeslot_id
-                        item2.room_id = timeslot_list[rand].room_code
+                        timeslot_list[rand].offering_id = item.offering_id
+                        timeslot_list[rand+1].offering_id = item.offering_id
+                        item.timeslot1_id = timeslot_list[rand].timeslot_id
+                        item.timeslot2_id = timeslot_list[rand+1].timeslot_id
+                        item.room_id = timeslot_list[rand].room_code
 
-                    '''
-                    timeslot_list[rand].offering_id = item2.offering_id
-                    item2.timeslot1_id = timeslot_list[rand].timeslot_id
-                    item2.timeslot2_id = timeslot_list[rand].timeslot_id
-                    '''
                 print("test")
                 boolean = True
             else:
                 boolean = False
+
+
+# Checks for offerings that weren't assigned properly
+
+
+# Checks for Faculty Schedule
     for test in offering_list:
         print(test.course_id, test.course_code, test.timeslot1_id, test.timeslot2_id, test.section, test.professor_id, test.room_id)
--- main.py
+++ main.py
@@ -26,21 +26,21 @@ general_course_list = list(load_general_course_list())
 
 # Print Test of Loading Functions
 print("Faculty List")
-for item3 in faculty_list:
-    print(item3.professor_id, item3.first_name, item3.last_name, item3.load)
-    for item6 in item3.preferred_courses:
-        print(item6)
+for item in faculty_list:
+    print(item.professor_id, item.first_name, item.last_name, item.load)
+    for item2 in item.preferred_courses:
+        print(item2)
 print("\n")
 
 print("Offering List")
-for item4 in offering_list:
-    print(item4.offering_id, item4.section, item4.course_id, item4.course_code, item4.units, item4.max_students, item4.course_type, item4.room_id, item4.flowchart_id)
+for item in offering_list:
+    print(item.offering_id, item.section, item.course_id, item.course_code, item.units, item.max_students, item.course_type, item.room_id, item.flowchart_id)
 print ("\n")
 
 
 print("General Course List")
-for item6 in general_course_list:
-    print(item6.course_code, item6.section, item6.faculty_name, item6.room, item6.start_year, item6.end_year, item6.term, item6.start_time, item6.end_time, item6.day1, item6.day2)
+for item in general_course_list:
+    print(item.course_code, item.section, item.faculty_name, item.room, item.start_year, item.end_year, item.term, item.start_time, item.end_time, item.day1, item.day2)
 
 
 for item in general_course_list:
@@ -51,8 +51,8 @@ for item in general_course_list:
             item2.offering_id = item.course_code + " " + item.section + " " + "GE"
 
 print("Timeslot List")
-for item5 in timeslot_list:
-    print(item5.timeslot_id, item5.room_id, item5.class_day, item5.begin_time, item5.end_time, item5.room_type, item5.room_code, item5.room_capacity, item5.offering_id)
+for item in timeslot_list:
+    print(item.timeslot_id, item.room_id, item.class_day, item.begin_time, item.end_time, item.room_type, item.room_code, item.room_capacity, item.offering_id)
 print ("\n")
 
 # Assigns Faculty to Offerings and Offerings to Timeslots
