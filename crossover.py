acca0b9513e167a6e385cf809324f9969da1851f
kevinkramer18 <kevin_kramer@dlsu.edu.ph>
2019-07-08 07:18:13 +0800

    1.) Did conditions for topparent crossover transfer but haven't completed the crossover

--- crossover.py
+++ crossover.py
@@ -9,7 +9,7 @@ def crossover_process(topParent, bottomParent, child):
 
         #Gives the professor's index from the topparent
         print(topParent.offerings[i].professor_id)
-        print(child.faculty[next(j for j, x in enumerate(child.faculty) if x.professor_id == topParent.offerings[i].professor_id)].professor_id)
+        #print(child.faculty[next(j for j, x in enumerate(child.faculty) if x.professor_id == topParent.offerings[i].professor_id)].professor_id)
         tpProfessorIndex = next(j for j, x in enumerate(child.faculty) if x.professor_id == topParent.offerings[i].professor_id)
         print(tpProfessorIndex)
 
@@ -20,18 +20,24 @@ def crossover_process(topParent, bottomParent, child):
 
         #tpt1begin_time gives the begin_time from the first parent timeslot
         print(next(j for j, x in enumerate(topParent.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot1_id))
-        print(str(topParent.timeslots[next(j for j, x in enumerate(topParent.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot1_id)].class_day) + '-' + str(topParent.timeslots[next(j for j, x in enumerate(topParent.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot1_id)].begin_time))
+        #print(str(topParent.timeslots[next(j for j, x in enumerate(topParent.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot1_id)].class_day) + '-' + str(topParent.timeslots[next(j for j, x in enumerate(topParent.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot1_id)].begin_time))
         tpt1begin_time = str(topParent.timeslots[next(j for j, x in enumerate(topParent.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot1_id)].class_day) + '-' + str(topParent.timeslots[next(j for j, x in enumerate(topParent.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot1_id)].begin_time)
         print(tpt1begin_time)
 
         #tpt2begin_time gives the begin_time from the second parent timeslot
         print(next(j for j, x in enumerate(topParent.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot2_id))
-        print(str(topParent.timeslots[next(j for j, x in enumerate(topParent.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot2_id)].class_day) + '-' + str(topParent.timeslots[next(j for j, x in enumerate(topParent.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot2_id)].begin_time))
+        #print(str(topParent.timeslots[next(j for j, x in enumerate(topParent.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot2_id)].class_day) + '-' + str(topParent.timeslots[next(j for j, x in enumerate(topParent.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot2_id)].begin_time))
         tpt2begin_time = str(topParent.timeslots[next(j for j, x in enumerate(topParent.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot2_id)].class_day) + '-' + str(topParent.timeslots[next(j for j, x in enumerate(topParent.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot2_id)].begin_time)
         print(tpt2begin_time)
 
+        #Returns whether the timeslot1 in child is occupied or not from topParent
+        print("*******************************")
+        ctimeslot1 = child.timeslots[next(j for j, x in enumerate(child.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot1_id)].offering_id
+        ctimeslot2 = child.timeslots[next(j for j, x in enumerate(child.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot2_id)].offering_id
+        print(next(j for j, x in enumerate(child.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot1_id))
+        print(next(j for j, x in enumerate(child.timeslots) if x.timeslot_id == topParent.offerings[i].timeslot2_id))
 
-        if rand <= 60 and tpt1begin_time not in child.faculty[tpProfessorIndex].schedule and tpt2begin_time not in child.faculty[tpProfessorIndex].schedule:
+        if rand <= 60 and tpt1begin_time not in child.faculty[tpProfessorIndex].schedule and tpt2begin_time not in child.faculty[tpProfessorIndex].schedule and ctimeslot1 == ""  and ctimeslot2 == "" and (child.faculty[tpProfessorIndex].units + topParent.offerings[i].units) <= child.faculty[tpProfessorIndex].load :
             child.offerings[i].professor_id = topParent.offerings[i].professor_id
             child.offerings[i].room_id = topParent.offerings[i].room_id
 
@@ -39,7 +45,7 @@ def crossover_process(topParent, bottomParent, child):
             child.offerings[i].timeslot2_id = topParent.offerings[i].timeslot2_id
             child.offerings[i].college_id = topParent.offerings[i].college_id
 
-            
+
 
 
 
